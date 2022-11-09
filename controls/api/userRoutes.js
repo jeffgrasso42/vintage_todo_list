596f027a1d34f1f4240c673f2b7b96feff8beb5f
JAG <jagrasso42@gmail.com>
2022-11-09 16:02:17 -0500

    login route works

--- controls/api/userRoutes.js
+++ controls/api/userRoutes.js
@@ -20,4 +20,35 @@ router.post('/', async (req, res) => {
   });
 });
 
+// POST /api/users/login - logs a user in
+router.post('/login', async (req, res) => {
+  // get user data from req.body
+  const { username, password } = req.body;
+  // create a new user
+  const user = await User.findOne({
+    where: { username: username },
+  });
+
+  // does the user exist
+  if (!user) {
+    // no? send back a 404
+    return res.status(404).json({ message: 'user not found' });
+  }
+
+  // is the password correct?
+  if (!user.checkPassword(password)) {
+    return res
+      .status(401)
+      .json({ message: 'username or password was incorrect' });
+  }
+  // no? send back 404
+
+  // add user info to the session
+  req.session.save(() => {
+    req.session.loggedIn = true;
+    req.session.userId = user.id;
+    // send user info back
+    res.status(200).json({ message: 'successfully logged in' });
+  });
+});
 module.exports = router;
