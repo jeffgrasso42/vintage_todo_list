2f0b3bbffb11fefdacdb0b2f223a7a34432611ff
JAG <jagrasso42@gmail.com>
2022-11-09 13:16:03 -0500

    Added dynamic port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 const myLogger = function (req, res, next) {
   console.log('LOGGED');
@@ -12,4 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
