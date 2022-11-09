f0287825dc2216f5f7faf9910686bd165397d741
JAG <jagrasso42@gmail.com>
2022-11-09 15:48:40 -0500

    create new user POST route working

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const session = require('express-session');
+require('dotenv').config();
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controls');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const myLogger = function (req, res, next) {
-  console.log('LOGGED');
-  next();
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
 };
 
-app.use(myLogger);
+app.use(session(sess));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
