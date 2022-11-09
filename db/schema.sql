0d08286aef402118b86a906e5b5cbe1251b44128
JAG <jagrasso42@gmail.com>
2022-11-09 14:00:44 -0500

    changed schema, added mysql2 dependency

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS vintage_todo_db;
+CREATE DATABASE vintage_todo_db;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +15,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+  });
 });
